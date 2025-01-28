15fd048be0ee75bdd4975aacbca6f7c0f7d456d6
OJ Abba <phavorabba@gmail.com>
2025-01-28 15:03:17 +0000

    [Fix] SC-162578 SC-123527 Fix `baseRequest`'s typing in `searchDevicesService`

--- src/services/simple-mdm/searchDevicesService.ts
+++ src/services/simple-mdm/searchDevicesService.ts
@@ -3,7 +3,7 @@ import type { IDeskproClient } from "@deskpro/app-sdk";
 import type { Device } from "./types";
 
 const searchDevicesService = (client: IDeskproClient, q: string) => {
-  return baseRequest<Device>(client, {
+  return baseRequest<Device[]>(client, {
     url: `/devices`,
     queryParams: {
       search: q,

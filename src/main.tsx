0bc812f5a3a2db7abd6cd74426f49543333803cf
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-06 13:57:41 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#42)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import './instrument';
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "./query";
 import { App } from "./App";
@@ -15,8 +15,11 @@ import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "./main.css";
 import "simplebar/dist/simplebar.min.css";
 import { Scrollbar } from "@deskpro/deskpro-ui";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render((
   <React.StrictMode>
     <Scrollbar style={{ height: "100%", width: "100%" }}>
@@ -24,7 +27,7 @@ root.render((
         <HashRouter>
           <QueryClientProvider client={queryClient}>
             <Suspense fallback={<LoadingSpinner />}>
-              <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <ErrorBoundary fallback={ErrorFallback}>
                 <App />
               </ErrorBoundary>
             </Suspense>
